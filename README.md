1b2b124e9a0021be28775cc86adfaf0a433aaf15
markvanderloo <mark.vanderloo@gmail.com>
2017-09-08 17:39:32 +0200

    fixed some stuff, first example runs

--- README.md
+++ README.md
@@ -3,3 +3,37 @@
 - Create attractive validation reports based on results from the [validate](https://cran.r-project.org/web/packages/validate/) package.
 - export validation results to [ESS JSON reporting standard](https://ec.europa.eu/eurostat/cros/system/files/20170815essnetvalidationwp2valreport_1.0.0.pdf) (pdf).
 - read validation results from ESS JSON reporting standard.
+
+### Example
+
+After cloning this repo, you can do the following.
+
+```r
+install.packages('validate')     # required for current work
+install.packages('jsonvalidate') # to check output
+
+devtools::load_all('pkg')
+library(jsonvalidate)
+
+data(retailers,package="validate")
+v <- validator(
+  turnover >= 0
+  , total.costs + profit == turnover
+  , mean(turnover,na.rm=TRUE) >= 0 
+)
+cf <- confront(retailers,v)
+dat <- ess_data_frame(cf,v)
+json <- ess_json(dat)
+cat(json)
+
+
+schema <- paste(
+  readLines("https://raw.githubusercontent.com/data-cleaning/ValidatReport/master/json/validation_report.json")
+  , collapse="\n")
+
+json_validate(json, schema)
+
+
+```
+
+
--- pkg/R/essreport.R
+++ pkg/R/essreport.R
@@ -1,7 +1,4 @@
-
-
-
-
+#' @import validate
 
 get_event <- function( time = as.character(Sys.time())
           , actor=NULL, agent="", trigger=""){
@@ -28,9 +25,10 @@ get_rule <- function(dat,language="R package validate 0.2.0"){
 }
 
 get_data <- function(dat, key){
+  src <- if (is.na(key)) "NA" else sprintf("%s",dat[,key])
   list(
-      source = dat[,key]
-    , target = if(is.null(dat$target)) dat[,key] else dat$target
+      source = src
+    , target = if(is.null(dat$target)) src else dat$target
     , description =  ""
   )
 }
@@ -39,29 +37,45 @@ get_data <- function(dat, key){
 #' 
 #' @param validation An object of class \code{\link[validate]{validation}}
 #' @param rules An object of class \code{\link[validate]{validator}}
+#' @param id An identifying key for the report
+#' @param ... extra columns, binded to the output with \code{cbind}
+#' 
 #' 
 #' @export
 #' @rdname ess_json
-ess_data_frame <- function(confrontation, rules, id = NULL , ...){
-  id <- if (is.null(id)) strftime(Sys.time(),"VALIDATION-%Y%m%dT%H:%M:%S") else id
+ess_data_frame <- function(validation, rules, id = NULL , ...){
   out <- merge(
     validate::as.data.frame(rules)
-    , validate::as.data.frame(confrontation))
+    , validate::as.data.frame(validation))
+  # key for validation result. Important only for aggregates
+  out$id <- sprintf("$05d",seq_len(nrow(out)))
   out <- cbind(out,...)
-  attr(out,"id") <- id
+  # name of column containing the data key
+  attr(out,"key") <- validation$._key
   out
 }
 
 
 #' @param dat Output of a call to \code{ess_data_frame}
 #' @export
+#' 
+#' @examples
+#' data(retailers,package="validate")
+#' v <- validator(
+#'   turnover >= 0
+#'   , total.costs + profit == turnover
+#'   , mean(turnover,na.rm=TRUE) >= 0 
+#' )
+#' cf <- confront(v, retailers)
+#' dat <- ess_data_frame(v, cf, id="my_validation")
+#' ess_json(dat)
 ess_json <- function(dat){
   event <- get_event()
   rule <- get_rule(dat)
-  data <- get_data(dat, key=dat$key)
+  data <- get_data(dat, key=attr(dat,"key"))
   
   json <- sprintf(validation_template()
-      , id
+      , id = dat$id
       # event
       , event$time
       , event$actor
@@ -78,11 +92,10 @@ ess_json <- function(dat){
       , data$target
       , data$description
       # value
-      , sprintf("%s", as.integer(d$value))
+      , sprintf("%s", as.integer(dat$value))
   )
   paste0("[", paste(json, collapse=","),"]")
 }
-
 
 
 validation_template <- function(x){
