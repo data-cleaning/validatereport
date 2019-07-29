01092f5e4e69b80ef8882ed77b26cb88c2ca0ffe
Mark van der Loo <mark.vanderloo@gmail.com>
2019-07-29 13:58:30 +0200

    added test

--- pkg/inst/tinytest/test_ess_report.R
+++ pkg/inst/tinytest/test_ess_report.R
@@ -1,8 +1,8 @@
 
-# minimal example
 library(validatereport)
 library(validate)
 
+# minimal example
 d <- data.frame(id = "00", x = 1)
 v <- validator( x >= 0 )
 cf <- confront(d,v, key="id")
@@ -12,7 +12,13 @@ expect_true(is_ess_validation_report( microdata_validation_report(cf, v) ))
 
 
 
-# Example with quotes
+# Example with double quotes in ruleset: these need explicit escaping when
+# stored in JSON format.
+d <- data.frame(id = "00", x = "a")
+v <- validator(x %in% c("a","b"))
+cf <- confront(d,v,key="id")
 
+expect_true(is_ess_validation_report( ess_validation_report(cf, v) ))
+expect_true(is_ess_validation_report( microdata_validation_report(cf, v) ))
 
 
